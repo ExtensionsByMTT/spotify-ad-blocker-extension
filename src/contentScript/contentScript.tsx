b85d821d634fcf5652649cd979d080962f42a978
unknown <karan.trippybug@gmail.com>
2023-09-26 14:20:08 +0530

    add popup ads code and new images

--- src/contentScript/contentScript.tsx
+++ src/contentScript/contentScript.tsx
@@ -1,11 +1,22 @@
-
-////////////////////////////////////////////////
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./contentScript.css";
 
-const App: React.FC<{}> = () => { 
-  
+const App: React.FC<{}> = () => {
+  const divs = document.getElementsByTagName("div");
+  for (const div of divs) {
+    const classesToCheck = [
+      "ad",
+      "promo",
+      "banner",
+      "GoogleActiveViewElement",
+      "desktopAd",
+    ];
+    if (classesToCheck.some((className) => div.classList.contains(className))) {
+      div.style.display = "none";
+    }
+  }
+
   return <></>;
 };
 
